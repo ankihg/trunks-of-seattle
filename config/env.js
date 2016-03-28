438c5a7f3bead0558bd8133b4cdb1cb479e7712b
ankihg <hagelin.annika@gmail.com>
2016-03-28 11:31:16 -0700

    db setup and tree db testing

--- config/env.js
+++ config/env.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.PORT = process.env.PORT || 3000;
-exports.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/db';
+// exports.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/db';
+exports.MONGO_URI = 'mongodb://sea-trunks:plztr33s@ds023088.mlab.com:23088/trunks-of-seattle';
 
 // TO-DO: replace mongodb with mongolab url
