1bccc56db377ed6c87fecfd14134a4d662a6fbb6
ankihg <hagelin.annika@gmail.com>
2016-03-29 08:50:46 -0700

    flickr uploads

--- models/Photo.js
+++ models/Photo.js
@@ -1,16 +1,15 @@
 'use strict';
+const fs = require('fs');
+
 const Flickr = require('flickrapi');
 const FlickrOptions = {
   api_key: process.env.FLICKR_API_KEY,
   secret: process.env.FLICKR_SECRET,
   user_id: process.env.FLICKR_USER_ID,
   access_token: process.env.FLICKR_ACCESS_TOKEN,
-  access_token_secret: process.env.FLICKR_ACCESS_TOKEN_SECRET
+  access_token_secret: process.env.FLICKR_ACCESS_TOKEN_SECRET,
+  permissions: 'write'
 };
-
-/* export FLICKR_USER_ID="141429933@N06"
-export FLICKR_ACCESS_TOKEN="72157666368248132-41204919f4b2768e"
-export FLICKR_ACCESS_TOKEN_SECRET="830d049b9f430504" */
 
 module.exports = (mongoose, models) => {
   let PhotoSchema = mongoose.Schema({
@@ -25,14 +24,16 @@ module.exports = (mongoose, models) => {
     var uploadOptions = {
       photos: [{
         title: 'plz',
-        photo: require('fs').readFileSync(__dirname+'/../storage/plz.jpg')
+        photo: fs.createReadStream(__dirname+'/../storage/plz.jpg')
       }]
     };
+    console.log(uploadOptions);
 
     Flickr.upload(uploadOptions, FlickrOptions, function(err, result) {
       if(err) {
         return console.error(error);
       }
+      console.log(result);
       console.log("photos uploaded", result);
     });
   });
